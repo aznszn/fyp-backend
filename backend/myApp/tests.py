f4027403026688b8eb7efc0dd9bf37c6605eab26
aznszn <akadri.bscs20seecs@student.nust.edu.pk>
2023-12-15 20:30:00 +0500

    aargs

--- backend/myApp/tests.py
+++ backend/myApp/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
 from scripts import accompaniment
+import demucs.separate
 
 accompaniment.main("sunshine30s.wav", length=30, drums='half-time')
--- backend/myApp/views.py
+++ backend/myApp/views.py
@@ -1,10 +1,14 @@
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
 from myApp.scripts import accompaniment
+import zipfile
+import tempfile
 import os
+from pydub import AudioSegment
+
 
 def helloWorld(request):
+    print("hello")
     return JsonResponse({'message': 'Hello, World!'})
 
 @csrf_exempt
@@ -18,20 +22,55 @@ def melodyGenerate(request):
         audio_file = request.FILES.get('audioFile')
         if audio_file:
             # Do something with the audio file, e.g., save it to disk
-            output_audio_path = 'myApp/inputs/' + str(audio_file)
+            output_audio_path = './myApp/inputs/' + str(audio_file)
             with open(output_audio_path, 'wb') as destination:
                 for chunk in audio_file.chunks():
                     destination.write(chunk)
 
             print('Audio file received and saved')
-            
-            # Process the audio file if needed (replace this with your processing logic)
-            accompaniment.main(str(audio_file), length=30, drums='half-time')
 
-            # Return the processed audio file as a response
-            with open('myApp/outputs/' + str(audio_file), 'rb') as audio_file:
-                response = HttpResponse(audio_file.read(), content_type='audio/wav')
-                response['Content-Disposition'] = 'attachment; filename="accompaintment.wav"'
+            print(instruments)
+            # Process the audio file if needed (replace this with your processing logic)
+            if 'drums' in instruments:
+                drums = 'normal'
+            elif 'double-time' in instruments:
+                drums = 'double-time'
+            elif 'half-time' in instruments:
+                drums = 'half-time'
+
+            piano = True if 'piano' in instruments else False
+            
+            accompaniment.main(str(audio_file), drums=drums, piano=piano, length=30)
+
+            file_base_name = str(audio_file)[:-4]
+            vocals = AudioSegment.from_file('./myApp/scripts/separated/htdemucs/' + file_base_name + '_vocals.wav')
+            ins = AudioSegment.from_file('./myApp/scripts/separated/htdemucs/' + file_base_name + '_no_vocals.wav')
+            accom = AudioSegment.from_file('./myApp/outputs/' + str(audio_file))
+
+            with tempfile.TemporaryDirectory() as temp_dir:
+                vocals_path = os.path.join(temp_dir, file_base_name + '_vocals.wav')
+                instruments_path = os.path.join(temp_dir, file_base_name + '_no_vocals.wav')
+                accompaniment_path = os.path.join(temp_dir, str(audio_file))
+
+                vocals.export(vocals_path, format='wav')
+                ins.export(instruments_path, format='wav')
+                accom.export(accompaniment_path, format='wav')
+
+                # Create a ZIP file containing the processed audio files
+                zip_buffer = tempfile.TemporaryFile()
+                with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
+                    zip_file.write(vocals_path, 'vocals.wav')
+                    zip_file.write(instruments_path, 'instruments.wav')
+                    zip_file.write(accompaniment_path, 'accompaniment.wav')
+
+                # Seek to the beginning of the buffer before sending the response
+                zip_buffer.seek(0)
+
+                # Return the ZIP file as a response
+                response = HttpResponse(zip_buffer.read(), content_type='application/zip')
+                response['Content-Disposition'] = 'attachment; filename="processed_audio.zip"'
+
+                print('Returning')
                 return response
 
         return JsonResponse({'error': 'Audio file not received'}, status=400)
